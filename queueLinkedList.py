78e598adc75d413245b4386a8f48bc3c760f6243
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2024-08-20 20:33:45 -0500

    Initial commit with queue methods, enqueue, print and dequeue

---
+++ queueLinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def enque(self, data):
+        newNode = Node(data)
+        if self.front is None:
+            self.front = newNode
+            return
+
+        temp = self.front
+        while temp.next:
+            temp = temp.next
+
+        temp.next = newNode
+
+    def printQueue(self):
+        if self.front is None:
+            print("The queue is empty")
+            return
+
+        temp = self.front
+        while temp:
+            print(temp.data, end='->' if temp.next else '\n')
+            temp = temp.next
+
+    def dequeue(self):
+        if self.front is None:
+            print('The queue is empty')
+            return
+
+        next = self.front.next
+        self.front = None
+        self.front = next
+
+
+queue = Queue()
+
+queue.enque(15)
+queue.enque(25)
+queue.enque(35)
+queue.enque(20)
+queue.enque(10)
+
+queue.printQueue()
+
+queue.dequeue()
+
+queue.printQueue()
