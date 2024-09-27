264c722e3270b9a0572cb777fd7d46b16bac07ba
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-09-27 17:19:22 -0500

    added comments

--- firstUniqueChar.py
+++ firstUniqueChar.py
@@ -1,3 +1,18 @@
+# Example 1:
+# Input: s = "leetcode"
+# Output: 0
+
+# Explanation:
+# The character 'l' at index 0 is the first character that does not occur at any other index.
+
+# Example 2:
+# Input: s = "loveleetcode"
+# Output: 2
+
+# Example 3:
+# Input: s = "aabb"
+# Output: -1
+
 class Solution:
     def uniqueChar(self, s: str):
         unique = set()
